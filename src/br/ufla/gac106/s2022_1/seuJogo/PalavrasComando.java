9c652a33d9d8486f10a73f0b9773f376240147ff
Maria de Souza Severino <mariarsseverino@gmail.com>
2023-02-24 18:06:26 -0300

    Mundança de tipos dos comandos para enumeradores

--- src/br/ufla/gac106/s2022_1/seuJogo/PalavrasComando.java
+++ src/br/ufla/gac106/s2022_1/seuJogo/PalavrasComando.java
@@ -1,9 +1,11 @@
 package br.ufla.gac106.s2022_1.seuJogo;
 
+import java.util.HashMap;
+
 /**
  * Esta classe é parte da aplicação "World of Zuul".
- * "World of Zuul" é um jogo de aventura muito simples, baseado em texto.  
- * 
+ * "World of Zuul" é um jogo de aventura muito simples, baseado em texto.
+ *
  * Essa classe guarda uma enumeração de todos os comandos conhecidos do
  * jogo. Ela é usada no reconhecimento de comandos como eles são digitados.
  *
@@ -11,21 +13,30 @@ package br.ufla.gac106.s2022_1.seuJogo;
  */
 
 public class PalavrasComando {
-    // um vetor constante que guarda todas as palavras de comandos válidas
-    private static final String[] comandosValidos = {
-        "ir", "sair", "ajuda"
-    };
 
-    /**
-     * Verifica se uma dada String é uma palavra de comando válida. 
-     * @return true se a string dada é um comando valido, false se não é.
-     */
-    public boolean ehComando(String umaString) {
-        for(int i = 0; i < comandosValidos.length; i++) {
-            if(comandosValidos[i].equals(umaString))
-                return true;
-        }
-        // se chegamos aqui, a string não foi encontrada nos comandos.
-        return false;
+  // um vetor constante que guarda todas as palavras de comandos válidas
+  //   private static final String[] comandosValidos = { "ir", "sair", "ajuda" };
+
+  // Um hashMap que guarda todas as palavras de comandos válidas
+  private HashMap<String, ComandoEnum> comandosValidos = new HashMap<String, ComandoEnum>();
+
+  /**
+   * Armazena os comandos validos
+   */
+  PalavrasComando() {
+    for (ComandoEnum comando : ComandoEnum.values()) {
+      comandosValidos.put(comando.toString(), comando);
     }
+  }
+
+  /**
+   * Verifica se uma dada String é uma palavra de comando válida.
+   * @return true se a string dada é um comando valido, false se não é.
+   */
+  public boolean ehComando(String umaString) {
+    if (comandosValidos.containsKey(umaString)) {
+      return true;
+    }
+    return false;
+  }
 }
