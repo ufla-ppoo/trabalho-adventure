d109cabff96745b24425f35fc7fcc7548f9a4d6d
vitorfn <vitorfnunes@gmail.com>
2023-02-25 16:55:41 -0300

    Padronização de código

--- src/br/ufla/gac106/s2022_1/seuJogo/App.java
+++ src/br/ufla/gac106/s2022_1/seuJogo/App.java
@@ -1,8 +1,9 @@
 package br.ufla.gac106.s2022_1.seuJogo;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Jogo jogo = new Jogo();		
-		jogo.jogar();
-    }
+
+  public static void main(String[] args) throws Exception {
+    Jogo jogo = new Jogo();
+    jogo.jogar();
+  }
 }
